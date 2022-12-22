824d51681442d6dd4bc3c8e330c97101d2116180
giuseppe materni <giuseppe.materni@gmail.com>
2022-12-22 23:17:14 +0100

    22-12-22 (23:17)

---
+++ app.js
@@ -0,0 +1,294 @@
+/*jshint esversion:11 */
+
+// console.log=()=>{};
+// console.eror=()=>{};
+
+
+const RELEASE = "0.1.1";
+const SW_NAME = "/ularead/sw.js";
+
+const ualog = function (...args) {
+  UaLog.log(...args);
+};
+
+const app_log = function (txt) {
+  // msg_prn(item1, txt);
+  UaLog.log_show(txt);
+};
+
+const app_info = function (sw_name, release, state) {
+  const txt = ` 
+  <span class="info">
+  <span>SW ${sw_name}</span>
+  <span>(${release})</span>
+  <span>${state.toUpperCase()}</span>
+  </span>
+  `;
+  // msg_prn(item0, txt);
+  UaLog.log_show(txt);
+};
+
+const error = (...args) => {
+  console.error('app\n');
+  console.error(...args);
+  console.error('\n');
+};
+
+"use strict"; //jshint ignore:line
+
+// const app_inst_id = "app_inst_id";
+
+// const open_install = () => {
+//   const msg = "Add to home screen";
+//   const html = ` <a>${msg}</a>`;
+//   const elm = document.createElement("div");
+//   document.body.appendChild(elm);
+//   elm.innerHTML = html;
+//   elm.classList.add("app_install");
+//   elm.id = app_inst_id;
+//   return elm.querySelector("a");
+// };
+
+// const close_install = () => {
+//   const elm = document.querySelector(app_inst_id);
+//   if (!!elm)
+//     elm.classList.remove(elm);
+// };
+
+
+// let deferredPrompt;
+// window.addEventListener('beforeinstallprompt', (evn) => {
+//   evn.preventDefault();
+//   const wnd_inst = open_install();
+//   console.log("platform:", evn.platforms);
+//   deferredPrompt = evn;
+//   wnd_inst.addEventListener('click', () => {
+//     deferredPrompt.prompt();
+//     deferredPrompt.userChoice.then((choiceResult) => {
+//       if (choiceResult.outcome === 'accepted') {
+//         close_install();
+//         window.reload();
+//         console.log('User accepted the A2HS prompt');
+//       } else {
+//         console.log('User dismissed the A2HS prompt');
+//       }
+//       deferredPrompt = null;
+//     });
+//   });
+// });
+
+
+let SW_STATE = "unregistred";
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register(SW_NAME)
+    .then((registration) => {
+      let sw = null;
+      if (registration.installing) {
+        sw = registration.installing;
+        SW_STATE = "installing";
+        console.log('installing');
+        console.log(`scope: ${registration.scope}`);
+      } else if (registration.waiting) {
+        sw = registration.waiting;
+        SW_STATE = "waiting";
+        console.log('waiting');
+      } else if (registration.active) {
+        sw = registration.active;
+        SW_STATE = "active";
+        console.log('active');
+        navigator.serviceWorker.onmessage = receiveMessage;
+      }
+      if (sw) {
+        sw.addEventListener("statechange", (e) => {
+          console.log(`statechange ${e.target.state}`);
+        });
+      }
+      app_info(SW_NAME, RELEASE, SW_STATE);
+    })
+    .catch((error) => {
+      alert(`Error(0) app.js\n${SW_NAME}\n ${error}`);
+    });
+} else {
+  alert(`Erroro(1) app.js \n${SW_NAME} not in navigator`);
+}
+
+///////////////////
+//gestione messaggi
+////////////////////
+
+
+const receiveMessage = function (event) {
+  let msg = event.data;
+  try {
+    const name = msg.name;
+    const methods = Object.getOwnPropertyNames(ReceiversMessage);
+    if (!methods.includes(name)) {
+      const s = JSON.stringify(msg);
+      const err = `ERROR receiveMessage\nmessage:${s} `;
+      alert(err);
+      return;
+    }
+    ReceiversMessage[name](msg);
+  }
+  catch (err) {
+    let j = JSON.stringify(msg);
+    const s = `${err}\nmsg:${j}`;
+    alert(s);
+  }
+};
+
+const ReceiversMessage = {
+  ualog: function (msg) {
+    const data = msg.data;
+    ualog(data);
+  }
+};
+
+const postMessage = function (msg) {
+  if (navigator.serviceWorker.controller) {
+    navigator.serviceWorker.controller.postMessage(msg);
+  } else {
+    alert(`Error(4) app.js ServiceWorker not activated \n ${SW_NAME}`);
+  }
+};
+
+
+///////////////////////////////////
+
+
+const testMsgLog = function () {
+  const fn = function (event) {
+    navigator.serviceWorker.onmessage = receiveMessage;
+    const msg = event.data;
+    const data = msg.data;
+    UaLog.log_show(data);
+  };
+  const msg = {
+    name: "testMsg",
+    ops: {},
+    data: "Test Msg Log"
+  };
+  navigator.serviceWorker.onmessage = fn;
+  postMessage(msg);
+};
+
+
+const toggleLogSW = function () {
+  const name = toggleLogSW.name;
+  const msg = {
+    name: name,
+    ops: {},
+    data: ""
+  };
+  postMessage(msg);
+};
+
+const cacheKeys = function (call) {
+  const fn = function (event) {
+    navigator.serviceWorker.onmessage = receiveMessage;
+    const msg = event.data;
+    const data = msg.data;
+    call(data);
+  };
+  const name = cacheKeys.name;
+  const msg = {
+    name: name,
+    ops: {},
+    data: ""
+  };
+  navigator.serviceWorker.onmessage = fn;
+  postMessage(msg);
+};
+
+const setCache = function (key, text, cacche_name = null) {
+  const msg = {
+    name: setCache.name,
+    ops: {
+      key: key,
+      cache_name: cacche_name
+    },
+    data: text
+  };
+  postMessage(msg);
+};
+
+const getCacheText = function (key, call, cache_name = null) {
+  const fn = function (event) {
+    navigator.serviceWorker.onmessage = receiveMessage;
+    const msg = event.data;
+    const data = msg.data;
+    call(data);
+  };
+  const msg = {
+    name: getCacheText.name,
+    ops: {
+      key: key,
+      cache_name: cache_name
+    },
+    data: ""
+  };
+  navigator.serviceWorker.onmessage = fn;
+  postMessage(msg);
+};
+
+const getCacheJson = function (key, call, cache_name = null) {
+  const fn = function (event) {
+    navigator.serviceWorker.onmessage = receiveMessage;
+    const msg = event.data;
+    const data = msg.data;
+    call(data);
+  };
+  const msg = {
+    name: getCacheJson.name,
+    ops: {
+      key: key,
+      cache_name: cache_name
+    },
+    data: ""
+  };
+  navigator.serviceWorker.onmessage = fn;
+  postMessage(msg);
+};
+
+
+////////////////////////////////////////////////////////
+
+function unregist() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister().then((boolean) => {
+        console.log("unregist:", boolean);
+      });
+    });
+  }
+}
+
+function unregistAll() {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+function clearCaches() {
+  caches.keys().then(function (names) {
+    for (let name of names)
+      caches.delete(name);
+  });
+}
+
+function reset() {
+  clearCaches();
+  unregist();
+  // app_info(SW_NAME, RELEASE, "unregistred");
+  window.location.reload();
+}
+
+function reload() {
+  window.location.reload();
+}
+
+
---
+++ sw.js
@@ -0,0 +1,404 @@
+/*jshint esversion:11 */
+
+// console.log=()=>{};
+
+const swlog = function (txt) {
+    console.log(txt);
+    if (clilog_active)
+        MessagePusher.ualog(txt);
+};
+
+const error = (...args) => {
+    console.error('\n');
+    console.error(...args);
+    console.error('\n');
+};
+
+"use strict";  //jshint ignore:line
+
+
+const CACHE_NAME_SW = "ulareac_1";
+let clilog_active = false;
+
+const config = {
+    version: "sw_3",
+    staticCacheItems: [
+        "/ularead/index.html",
+        "/ularead/css/ula.css",
+        "/ularead/js/ula.min.js",
+        "/ularead/app.js",
+        "/ularead/icons/maskable_icon_x512.png",
+        "/ularead/icons/maskable_icon_x384.png",
+        "/ularead/icons/maskable_icon_x192.png",
+        "/ularead/icons/maskable_icon_x144.png",
+        "/ularead/icons/icon07_x512.png",
+        "/ularead/icons/icon07_x384.png",
+        "/ularead/icons/icon07_x192.png",
+        "/ularead/icons/icon07_x144.png",
+        "/ularead/imgs/disegno800x590.png",
+        "/ularead/imgs/giardino800x480.jpg",
+        "/ularead/imgs/molo1280x680.jpg",
+
+        "/ularead/data/text_list.txt",
+        "/ularead/data/tr_gre_000.form.csv",
+        "/ularead/data/tr_gre_000.token.csv",
+
+        "/ularead/favicon.ico"
+    ]
+};
+
+self.addEventListener('install', (event) => {
+    swlog("install " + config.version);
+    event.waitUntil(caches.open(CACHE_NAME_SW)
+        .then((cache) => cache.addAll(config.staticCacheItems))
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    swlog("activate " + config.version);
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((cache_key) => {
+                    if (cache_key !== CACHE_NAME_SW) {
+                        return caches.delete(cache_key);
+                    }
+                })
+            );
+        }).then(() => {
+            swlog("Old caches are cleared!");
+            return self.clients.claim();
+        })
+    );
+});
+
+// TODO log gestione errori 
+// self.addEventListener('error', function (e) {
+//     console.error("XXX_", e);
+// });
+
+///////////////////////
+// fetch
+///////////////////////
+const SW_N = "n"; //networkOnly
+const SW_C = "c"; //cacheOnly
+const SW_NC = "nc"; //networkFirs
+const SW_CN = "cn"; //cacheFirest
+const SW_SVR = "svr"; //staleWhileRevalidate
+
+const STRATEGY_NAME = {
+    "n": "nwtrworkOnly",
+    "c": "cacheOnly",
+    "nc": "nwtrworkFirst",
+    "cn": "cacheFirst",
+    "svr": "staleWhileRevalidate"
+};
+
+/*
+destination:
+audio, audioworklet, document, embed, font, frame, 
+iframe, image, manifest, object, paintworklet, 
+report, script, sharedworker, style, track, video, worker 
+or xslt strings or the empty string.
+*/
+const setStrategy = function (rqs) {
+    const url = new URL(rqs.url);
+    // "hostname:" + url.hostname);
+    // "host:" + url.host);
+    // "port:" + url.port);
+    // "origin:" + url.origin);
+    const dest = rqs.destination;
+    const mode = rqs.mode;
+    // const headers = rqs.headers;
+    // const cache = rqs.cache;
+
+    let strategy = SW_NC;
+    if (["document", "image", "audio", "manifest"].includes(dest))
+        strategy = SW_CN;
+    else if (url.pathname.endsWith(".mp3"))
+        strategy = SW_CN;
+    else if (["script", "style"].includes(dest))
+        strategy = SW_SVR;
+    else if (url.pathname.endsWith(".less"))
+        strategy = SW_SVR;
+    else if (url.pathname.endsWith(".json"))
+        strategy = SW_NC;
+    else if (url.pathname.endsWith(".txt"))
+        strategy = SW_NC;
+    else if (mode == "cors")
+        strategy = SW_NC;
+    return {
+        "url": url.pathname,
+        "destination": dest,
+        "mode": mode,
+        // "headers": JSON.stringify(headers),
+        // "methos": rqs.method,
+        // "cahe": cache,
+        "strategy": strategy,
+        "strategy_name": STRATEGY_NAME[strategy]
+    };
+};
+
+self.addEventListener('fetch', (event) => {
+    const rqs_info = setStrategy(event.request);
+    const strategy = rqs_info.strategy;
+
+    //TODO log strategy
+    console.log(JSON.stringify(rqs_info, null, 4));
+
+    if (strategy == SW_N)
+        return networkOnly(event);
+    else if (strategy == SW_NC)
+        return networkFirst(event);
+    else if (strategy == SW_CN)
+        return cacheFirst(event);
+    else if (strategy == SW_SVR)
+        return staleWhileRevalidate(event);
+    else {
+        error("XXX_0 fetch null");
+        return;
+    }
+});
+
+const networkOnly = (event) => {
+    event.respondWith(fetch(event.request));
+};
+
+const networkFirst = (event) => {
+    event.respondWith(fetch(event.request)
+        .then((networkResponse) => {
+            console.log("networkFirst net");
+            return caches.open(CACHE_NAME_SW)
+                .then((cache) => {
+                    cache.put(event.request, networkResponse.clone());
+                    console.log("networkFirst cahae.put");
+                    return networkResponse;
+                });
+        })
+        .catch(() => {
+            console.log("networkFirst cache");
+            return caches.match(event.request);
+        })
+    );
+};
+
+/*
+cahe
+else
+network => cache.puts
+*/
+const cacheFirst = (event) => {
+    event.respondWith(caches.open(CACHE_NAME_SW)
+        .then((cache) => {
+            return cache.match(event.request.url)
+                .then((cachedResponse) => {
+                    if (cachedResponse) {
+                        console.log("cacheFirst cache");
+                        return cachedResponse;
+                    }
+                    return fetch(event.request)
+                        .then((fetchedResponse) => {
+                            console.log("cacheFirst net ");
+                            if (fetchedResponse.status == 200) {
+                                cache.put(event.request, fetchedResponse.clone());
+                                console.log("cacheFirst cahae.put");
+                            } else {
+                                error("XXX_1 fetchFirst cache.put response:", fetchedResponse);
+                            }
+                            return fetchedResponse;
+                        }).catch((error) => {
+                            error("XXX_1 fetchFirst ", error);
+                            return null;
+                        });
+                });
+        }));
+};
+
+//TODO come è possibile
+const cacheOnly = (event) => {
+    event.respondWith(caches.open(CACHE_NAME_SW)
+        .then((cache) => {
+            return cache.match(event.request.url)
+                .then((cachedResponse) => {
+                    if (cachedResponse) {
+                        console.log("cacheOnly");
+                        return cachedResponse;
+                    }
+                });
+        }));
+};
+
+/*
+cache first and network update cache 
+else 
+network and update cache)
+/*
+cache => network => cache.put
+else
+network => cache.put
+*/
+const staleWhileRevalidate = (event) => {
+    const url = event.request.url;
+    event.respondWith(caches.open(CACHE_NAME_SW)
+        .then((cache) => {
+            return cache.match(event.request.url)
+                .then((cachedResponse) => {
+                    if (cachedResponse) {
+                        console.log("staleWhileRevalidate cache");
+                        //TODO gestire errore in modalita on offline ??
+                        fetch(event.request)
+                            .then((networkResponse) => {
+                                cache.put(event.request, networkResponse.clone());
+                            }).catch((error) => {
+                                error(`"XXX_2 staleWhileRevalidate\n${url}\n`, error);
+                            });
+
+                        return cachedResponse;
+                    }
+                    else {
+                        console.log("staleWhileRevalidate net");
+                        return fetch(event.request)
+                            .then((networkResponse) => {
+                                cache.put(event.request, networkResponse.clone());
+                                return networkResponse;
+                            }).catch((error) => {
+                                e.error(`"XXX_3 staleWhileRevalidate\n${url}\n`, error);
+                            });
+                    }
+                });
+        }));
+};
+
+/////////////////////
+//cache setter/getter
+//////////////////////
+
+const CacheManager = {
+    set: function (key, data, name = null) {
+        const cache_name = name || CACHE_NAME_SW;
+        caches.open(cache_name)
+            .then((cache) => {
+                const rqs = new Request(key);
+                const rsp = new Response(data);
+                cache.put(rqs, rsp);
+            });
+    },
+    // const ops = {
+    //     ignoreSearch: true,
+    //     ignoreMethod: true,
+    //     ignoreVary: true
+    // };
+    getText: function (key, name = null) {
+        const cache_name = name || CACHE_NAME_SW;
+        return caches.open(cache_name).then((cache) => {
+            return cache.match(key);
+        }).then((response) => {
+            return response.text();
+        }).catch(() => {
+            return "";
+        });
+    },
+    getJson: function (key, name = null) {
+        const cache_name = name || CACHE_NAME_SW;
+        return caches.open(cache_name).then((cache) => {
+            return cache.match(key);
+        }).then((response) => {
+            return response.json();
+        }).catch(() => {
+            return {};
+        });
+    },
+    keys: function () {
+        return caches.open(CACHE_NAME_SW).then((cache) => {
+            return cache.keys();
+        }).then((requests) => {
+            const keys = [];
+            for (let rqs of requests)
+                keys.push(rqs.url);
+            return keys;
+        });
+    }
+};
+
+//////////////////////
+// gestione messaggi
+///////////////////////
+self.addEventListener('message', (event) => {
+    const msg = event.data;
+    const name = msg.name;
+    try {
+        // console.log("msg:" + JSON.stringify(msg));
+        MessageResponder[name](msg, event);
+    }
+    catch (err) {
+        const s = JSON.stringify(msg);
+        const es = `${err}\nmsg:${s}`;
+        error(es);
+    }
+});
+
+const MessageResponder = {
+    testMsg: function (msg, event) {
+        swlog("testMsgLog");
+        msg.data = `received from client ${msg.data}`;
+        event.source.postMessage(msg);
+    },
+    toggleLogSW: function () {
+        clilog_active = !clilog_active;
+    },
+    cacheKeys: async function (msg, event) {
+        swlog("cacheKeys");
+        const urls = await CacheManager.keys();
+        msg.data = urls;
+        event.source.postMessage(msg);
+    },
+    getCacheJson: async function (msg, event) {
+        swlog("getCacheJson");
+        const key = msg.ops.key;
+        const json = await CacheManager.getJson(key);
+        msg.data = json;
+        event.source.postMessage(msg);
+    },
+    setCache: function (msg) {
+        swlog("setCache");
+        const key = msg.ops.key;
+        const data = msg.data;
+        CacheManager.set(key, data);
+    },
+    getCacheText: function (msg, event) {
+        swlog("getCache");
+        const key = msg.ops.key;
+        CacheManager.getText(key)
+            .then((text) => {
+                msg.data = text;
+                event.source.postMessage(msg);
+            });
+    }
+};
+
+const MessagePusher = {
+    postMessage: (message) => {
+        return self.clients.matchAll().then(clients => {
+            clients.forEach((client) => {
+                client.postMessage(message);
+            });
+        });
+    },
+    ualog: function (text) {
+        const msg = {
+            name: "ualog",
+            ops: null,
+            data: text
+        };
+        this.postMessage(msg);
+    }
+};
+
+
+
+
+
+
+
+
